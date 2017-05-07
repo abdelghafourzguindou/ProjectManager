2d563d4a60fe828e9a13c48370ba2b65437b5398
ZGUINDOU Abdelghafour <zguindou@gmail.com>
2017-05-07 17:00:24 +0100

    Commit v3.0

--- src/com/zGuindou/ProjetManager/presentation/ProjectBean.java
+++ src/com/zGuindou/ProjetManager/presentation/ProjectBean.java
@@ -1,11 +1,16 @@
 package com.zGuindou.ProjetManager.presentation;
 
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
 
 import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ManagedBean
 @RequestScoped
@@ -18,6 +23,44 @@ public class ProjectBean {
 	}
 	
 	private String title;
+	private String description;
+	private String budget;
+	private String type;
+	private String active;
+
+	private List<SelectItem> typeList;
+
+	public String getActive() {
+		return active;
+	}
+
+	public void setActive(String active) {
+		this.active = active;
+	}
+
+	public List<SelectItem> getTypeList() {
+		return typeList;
+	}
+
+	public void setTypeList(List<SelectItem> typeList) {
+		this.typeList = typeList;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getBudget() {
+		return budget;
+	}
+
+	public void setBudget(String budget) {
+		this.budget = budget;
+	}
 
 	public String getTitle() {
 		return title;
@@ -26,11 +69,31 @@ public class ProjectBean {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	public void saveData(ActionEvent e) {
 		log.info("Title is : " + title);
 	}
-	
+
+	@PostConstruct
+	public void initBean() {
+		typeList = new ArrayList<>();
+		typeList.add(new SelectItem("", ""));
+		typeList.add(new SelectItem(1, "Informatique"));
+		typeList.add(new SelectItem(2, "Commerce"));
+		typeList.add(new SelectItem(3, "Math"));
+	}
+
+	public void addProject(ActionEvent e) {
+		log.error("Les valeurs : ");
+	}
 	
 
 }
